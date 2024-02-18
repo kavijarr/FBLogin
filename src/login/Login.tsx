b12ae3b24bf36abdc629d97e19eba9112d3b0bd7
kavijarr <kavijakumuditha12@gmail.com>
2024-02-18 09:18:46 +0530

    installing bootstrap and adding login tsx

---
+++ src/login/Login.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+function Login(){
+    return(
+        <div>
+            <div className="container-fluid"></div>
+        </div>
+    );
+}
+
+export default Login;
